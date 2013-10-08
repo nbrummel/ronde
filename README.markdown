21c036bd8d99a70a7705b252356ded07ebc5220f
saasbook <saasbook@saasbook.(none)>
2013-10-08 02:02:10 -0700

    Empty rails app.

---
+++ README.markdown
@@ -0,0 +1,3 @@
+Ronde is a fast and fun way to meet with friends and family.
+
+Post an update about where you're going, find friends who are doing the same thing or send invites to have them join - it's that easy. It's a new way to experience the world.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ronde::Application.config.secret_token = '9c2265351878723290faddcd8db9f24a6d9508e96a0d91ebd33363065decf0660abec574283a9408c08f58fae17f353f95d652a28cf348776ff4bb84dce5c6b0'
