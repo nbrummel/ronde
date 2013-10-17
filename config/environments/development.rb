f4fa5cede32461327e7a77b1bbe931aaa2b6a5f3
saasbook <saasbook@saasbook.(none)>
2013-10-16 23:22:28 -0700

    Added Devise gem. Added haml gem. Updated User db schema. Added default routes from Devise. Added static_controller for static_pages. Added root route. Configured devise to add a User using simple fields.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,10 @@ Ronde::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  # Default url options
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' } # disable for production
+  # config.action_mailer.default_url_options = { :host => 'http://ronde.herokuapp.com' } # enable for production
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
