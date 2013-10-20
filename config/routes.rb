427b13e2fbc910e4719f981490993fd7c9f4a18a
saasbook <saasbook@saasbook.(none)>
2013-10-20 16:16:45 -0700

    updated home and created contact and terms of use views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Ronde::Application.routes.draw do
   get "static/about"
   get "static/tour"
   get "static/home"
+  get "static/terms_of_use"
+  get "static/contact"
 
   devise_for :users, :controllers => { :omniauth_callbacks =>  "omniauth_callbacks" }
 
