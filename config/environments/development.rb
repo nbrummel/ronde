476cce4c4eadd77d6270ce381055a6e5c8e338d8
saasbook <saasbook@saasbook.(none)>
2013-10-20 18:55:50 -0700

    Ready for deploy.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,5 @@ Ronde::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,9 @@ Ronde::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  config.assets.initialize_on_precompile = false
+  config.action_mailer.default_url_options = { :host => 'http://ronde.herokuapp.com' }
+
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
