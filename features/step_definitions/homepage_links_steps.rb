f7d2222879da496bb4d6837c8af371a10c3ed738
corymc <cmcdowell@berkeley.edu>
2013-10-27 21:40:29 -0700

    homepage_link steps

---
+++ features/step_definitions/homepage_links_steps.rb
@@ -0,0 +1,15 @@
+Given /I am on the ronde homepage/ do
+	visit path_to('the Ronde homepage')
+end
+
+And /I click the "(.*)" link$/ do |link|
+	click_link(link)
+end
+
+And /I should be on the ronde "(.*)" page$/ do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
