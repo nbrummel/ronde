9a1f7bd50fece85c0b4f9a7e948ed12a56e3d66e
saasbook <saasbook@saasbook.(none)>
2013-11-03 13:31:05 -0800

    Events, invitations, and users invited users to events works very nicely so far. Testing to come.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,12 @@
 class Event < ActiveRecord::Base
-	belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-	has_many :invitations
-	has_many :attendees, :through => :invitations, :source => :friend
 
-	validates_presence_of :description, :location, :name, :start
-	attr_accessible :description, :end, :location, :name, :public, :start
+	# Associations
+	has_many :invitations
+	belongs_to :created_by, :class_name => 'User'
+	has_many :attending_users, :through => :invitations, :source => :user, :conditions => "status = 'confirmed'"
+
+	validates_presence_of :description, :location, :name, :start, :created_by
+	attr_accessible :description, :end, :location, :name, :public, :start, :created_by, :event_type, :user_id
 	after_initialize :init 
 
 	def init
--- db/migrate/20131031001556_create_invitations.rb
+++ db/migrate/20131031001556_create_invitations.rb
@@ -2,16 +2,11 @@ class CreateInvitations < ActiveRecord::Migration
   def change
     create_table :invitations do |t|
       t.integer :user_id
-      t.integer :friend_id
+      t.integer :invited_user_id
       t.integer :event_id
       t.string :status
       t.datetime :accepted_at
-
       t.timestamps
     end
-  end
-
-  def down
-  	remove_column :attending
   end
 end
