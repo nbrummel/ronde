25e66bb6c6774f32ad2cd23bd538797298792678
nbrummel <nathanbrummel@gmail.com>
2013-11-11 22:35:34 -0800

    added modal for the account page and made the edit page look a little better

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Ronde::Application.routes.draw do
   devise_scope :user do 
     get "/logout", :to => "devise/sessions#destroy" # Add a custom sign in route for user sign in
     get "/register", :to => "devise/registrations#new" # Add a Custom Route for Registrations
-    get "/profile/edit", :to => "devise/registrations#edit"
+    get "/user/:id/profile/edit", :to => "devise/registrations#edit"
     get "/profile/password", :to => "devise/passwords#edit"
   end
 
