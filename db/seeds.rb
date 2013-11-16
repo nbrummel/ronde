d07c1189c63d0c4faefbf42f70c544edfc0aadab
nbrummel <nathanbrummel@gmail.com>
2013-11-16 15:37:30 -0800

    added seed.rb so we can seed the db.  It is in the db folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+users = [{:email => 'nnn@gmail.com', :password => 'foobaraa', :password_confirmation => 'foobaraa', :first_name => 'nn', :last_name => 'yy'},
+		{:email => 'a@gmail.com', :password => 'foobaraa', :password_confirmation => 'foobaraa', :first_name => 'an', :last_name => 'ay'},
+		{:email => 'b@gmail.com', :password => 'foobaraa', :password_confirmation => 'foobaraa', :first_name => 'bn', :last_name => 'by'},
+		{:email => 'c@gmail.com', :password => 'foobaraa', :password_confirmation => 'foobaraa', :first_name => 'cn', :last_name => 'cy'},
+		{:email => 'd@gmail.com', :password => 'foobaraa', :password_confirmation => 'foobaraa', :first_name => 'dn', :last_name => 'dy'}
+	]
+
+users.each do |user|
+	User.create!(user)
+end
