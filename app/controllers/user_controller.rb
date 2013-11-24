b0329e88f626d4d8a01548b1b3932236ecb60570
corymc <cmcdowell@berkeley.edu>
2013-11-24 13:34:56 -0800

    added before_filter to user_controller to avoid code repetition

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,13 @@
 class UserController < ApplicationController
 	before_filter :check_login
+	before_filter :get_user_and_friend
+
+	def get_user_and_friend
+		@user = User.find_by_id(params[:id])
+		if params[:friend_id]
+			@friend = User.find_by_id(params[:friend_id])
+		end
+	end
 
 	def check_login
 		if not current_user
@@ -8,7 +16,6 @@ class UserController < ApplicationController
 	end
 
 	def friends
-		@user = User.find_by_id(params[:id])
 		@friends = @user.accepted_friends
 		if @friends.empty?
 			@friends = false
@@ -21,7 +28,6 @@ class UserController < ApplicationController
 	def search_friend
 		@user = User.find_by_id(params[:id])
 		if params[:searched_friend] != ""
-			# @possible_friends = User.find_all_by_email(params[:searched_friend])
 			@possible_friends = (User.where("email LIKE ?", "%#{params[:searched_friend]}%") + 
 								User.where("first_name LIKE ?", "%#{params[:searched_friend]}%") + 
 								User.where("last_name LIKE ?", "%#{params[:searched_friend]}%") + 
@@ -35,29 +41,21 @@ class UserController < ApplicationController
 		end
 	end
 	def add_friend
-		@user = User.find_by_id(params[:id])
-		@friend = User.find_by_id(params[:friend_id])
 		Friendship.request(@user, @friend)
 		flash[:friend_request_msg] = "You have successfully sent #{@friend.first_name} #{@friend.last_name} a friend request."
 		redirect_to "/user/#{params[:id]}/friends"
 	end
 	def accept_friend
-		@user = User.find_by_id(params[:id])
-		@friend = User.find_by_id(params[:friend_id])
 		Friendship.accept(@user, @friend)
 		flash[:accept_friend_msg] = "You and #{@friend.first_name} #{@friend.last_name} are now friends."
 		redirect_to "/user/#{params[:id]}/friends"
 	end
 	def remove_friend
-		@user = User.find_by_id(params[:id])
-		@friend = User.find_by_id(params[:friend_id])
 		Friendship.remove(@user, @friend)
 		flash[:remove_friend_msg] = "You have removed #{@friend.first_name} #{@friend.last_name} from your friends."
 		redirect_to "/user/#{params[:id]}/friends"
 	end
 	def reject_friend
-		@user = User.find_by_id(params[:id])
-		@friend = User.find_by_id(params[:friend_id])
 		Friendship.remove(@user, @friend)
 		redirect_to "/user/#{params[:id]}/friends"
 	end
