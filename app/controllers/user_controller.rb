0bcb665234626fba6572ec0f9cd50ec0e41ef51f
Nathan Brummel <sabersimon@gmail.com>
2013-11-24 20:56:19 -0800

    added redirect_to self.toFriends to eliminate duplicate code

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -42,20 +42,25 @@ class UserController < ApplicationController
 	def add_friend
 		Friendship.request(@user, @friend)
 		flash[:friend_request_msg] = "You have successfully sent #{@friend.first_name} #{@friend.last_name} a friend request."
-		redirect_to "/user/#{params[:id]}/friends"
+		redirect_to self.toFriends
 	end
 	def accept_friend
 		Friendship.accept(@user, @friend)
 		flash[:accept_friend_msg] = "You and #{@friend.first_name} #{@friend.last_name} are now friends."
-		redirect_to "/user/#{params[:id]}/friends"
+		redirect_to self.toFriends
 	end
 	def remove_friend
 		Friendship.remove(@user, @friend)
 		flash[:remove_friend_msg] = "You have removed #{@friend.first_name} #{@friend.last_name} from your friends."
-		redirect_to "/user/#{params[:id]}/friends"
+		redirect_to self.toFriends
 	end
+	
 	def reject_friend
 		Friendship.remove(@user, @friend)
-		redirect_to "/user/#{params[:id]}/friends"
+		redirect_to self.toFriends
+	end
+
+	def toFriends
+		"/user/#{params[:id]}/friends"
 	end
 end
